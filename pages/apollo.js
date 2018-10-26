41cf8a54795105e97985974848c9f446d2798025
Damien Varron <damien@varron.fr>
2018-10-25 18:27:18 -0700

    Add fetchMore

--- pages/apollo.js
+++ pages/apollo.js
@@ -22,23 +22,58 @@ class Apollo extends Component {
           value={keyword}
           onChange={this.onChange}
         />
-        <ul>
-          <Query query={QUERY} variables={{ keyword }} skip={!keyword}>
-            {({
-              loading,
-              error,
-              data: { search: { results = [] } = {} } = {}
-            }) => {
-              if (loading) return <li>Loading...</li>;
-              if (error) return <li>{error.message}</li>;
-              return results.map(({ id, name }) => <li key={id}>{name}</li>);
-            }}
-          </Query>
-        </ul>
+        <Query query={QUERY} variables={{ keyword, page: 1 }} skip={!keyword}>
+          {({
+            loading,
+            error,
+            variables,
+            data: { search: { results = [] } = {} } = {},
+            fetchMore
+          }) => {
+            if (loading) return <li>Loading...</li>;
+            if (error) return <li>{error.message}</li>;
+            return (
+              <>
+                <button
+                  type="button"
+                  onClick={() =>
+                    fetchMore({
+                      variables: { keyword, page: variables.page + 1 },
+                      updateQuery: (prev, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                          ...prev,
+                          search: {
+                            ...prev.search,
+                            results: [
+                              ...prev.search.results,
+                              ...fetchMoreResult.search.results
+                            ]
+                          }
+                        };
+                      }
+                    })
+                  }
+                >
+                  Fetch More
+                </button>
+                <ul>
+                  {results.map(({ id, name }) => (
+                    <li key={id}>{name}</li>
+                  ))}
+                </ul>
+              </>
+            );
+          }}
+        </Query>
         <style jsx>{`
           input {
             font-size: 22px;
             width: 500px;
+            padding: 14px;
+          }
+          button {
+            font-size: 22px;
             padding: 14px;
           }
         `}</style>
--- src/queries.js
+++ src/queries.js
@@ -4,9 +4,9 @@ const apiKey = "e1c030c90262b41deac61b0565e22368";
 const params = `api_key=${apiKey}&language=en-US`;
 
 export const QUERY = gql`
-  query Search($keyword: String!) {
-    search(keyword: $keyword)
-    @rest(type: "Search", path: "/search/tv?${params}&query={args.keyword}&page=1") {
+  query Search($keyword: String!, $page: Int!) {
+    search(keyword: $keyword, page: $page)
+    @rest(type: "Search", path: "/search/tv?${params}&query={args.keyword}&page={args.page}") {
       results @type(name: "Result") {
         id @export(as: "id")
         name
